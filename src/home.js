d9db2f738142abd425eadf7e3d68936872248fc7
Gerard <gerardbriangueco@gmail.com>
2020-11-10 16:49:40 +0800

    first commit

---
+++ src/home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <Route
+        exact
+        path="/"
+        render={() => (window.location = "https://www.facebook.com/ ")}
+      />
+    );
+  }
+}
