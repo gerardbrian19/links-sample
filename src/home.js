3331d29a690f202daf57af3e35f600f64996abd7
Gerard <gerardbriangueco@gmail.com>
2020-11-10 16:52:28 +0800

    added to link profile

--- src/home.js
+++ src/home.js
@@ -7,7 +7,7 @@ export default class Home extends Component {
       <Route
         exact
         path="/"
-        render={() => (window.location = "https://www.facebook.com/ ")}
+        render={() => (window.location = "https://www.facebook.com/xogerard ")}
       />
     );
   }
